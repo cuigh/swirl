2ead68fc8542118b05a2d4d20385d0888dd8f88b
cuigh <noname@live.com>
2018-07-02 18:04:38 +0800

    Bump version to 0.8.1

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	misc.BindOptions()
 
 	app.Name = "Swirl"
-	app.Version = "0.8.0"
+	app.Version = "0.8.1"
 	app.Desc = "A web management UI for Docker, focused on swarm cluster"
 	app.Action = func(ctx *app.Context) {
 		err := config.UnmarshalOption("swirl", &misc.Options)
